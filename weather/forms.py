37c9af3b628583f2c971de411d131f748650bedf
Alberto Senna Dias Neto <albertosdneto@gmail.com>
2022-11-20 19:37:30 -0300

    update form
    Add captcha, add custom field for captcha, update index template

--- weather/forms.py
+++ weather/forms.py
@@ -1,14 +1,21 @@
-from django.forms import ModelForm, TextInput
+from django.forms import ModelForm, TextInput, MultiWidget
 from .models import City
+from captcha.fields import CaptchaField, CaptchaTextInput
+
+
+class CustomCaptchaTextInput(CaptchaTextInput):
+    template_name = 'weather/custom_field.html'
 
 
 class CityForm(ModelForm):
+    captcha = CaptchaField(widget=CustomCaptchaTextInput, label='Please enter the characters in the image')
+
     class Meta:
         model = City
         fields = ['name']
         widgets = {
             'name': TextInput(
-                attrs={'class': 'form-control',
+                attrs={'class': 'form-control mb-2',
                        'placeholder': 'City Name',
                        'aria-label': 'Enter email address...',
                        'data-sb-validations': "required,email",
